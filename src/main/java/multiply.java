ba4e1b2c22f5a8adad64903ea8fc1317d276cf24
Om Gupta om <techom20@gmail.com>
2023-08-07 17:00:01 +0530

    added multiply.java

---
+++ src/main/java/multiply.java
@@ -0,0 +1,12 @@
+
+public class multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=5;
+		int b=7;
+		System.out.println("The mul is"+a*b);
+	}
+
+}
